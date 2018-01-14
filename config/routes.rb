b3c4db5a8c826a8a6b2ca3994500fb9643328172
meutley <matthew.utley.dev@gmail.com>
2018-01-14 12:39:07 -0600

    Add base navigation layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
   
+  get 'blood_pressure', to: 'blood_pressure#index'
   get 'blood_pressure/index'
   get 'blood_pressure/new'
   post 'blood_pressure/new'
