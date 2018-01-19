bac92eede4b66544ec6d46f5caa1f88e676e6e93
meutley <matthew.utley.dev@gmail.com>
2018-01-18 20:46:25 -0600

    Finish new user preferences functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
         tz = current_user&.timezone
         return value&.in_time_zone(tz).strftime("%-m/%-d/%Y %I:%M %p") if !tz.nil?
     end
+
+    # Static data
+    def weight_units
+        return ["lbs", "kg"]
+    end
+
+    def blood_glucose_units
+        return ["mg/dL", "mmol/L"]
+    end
 end
